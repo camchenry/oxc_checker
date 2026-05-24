f2ddc3489cc48b306291e3ada0c32e0beb6ebbbe
Cam McHenry <camchenry@users.noreply.github.com>
2026-05-23 22:51:18 -0400

    Add more generic inference cases

--- tests/conformance/cases/compiler/basicFunctionGenericInference.ts
+++ tests/conformance/cases/compiler/basicFunctionGenericInference.ts
@@ -6,3 +6,10 @@ function foo<T>(x: T) {
 const x: string = "hello"
 const x1 = foo(x);
 const x2 = foo(x1);
+
+function foo2<T, U>(x: T, y: U) {
+  return { x, y };
+}
+
+const y = foo2("hello", 123);
+const y2 = foo2(y.x, y.y);
