d8f81b532ecee343a8c1cb298b315f4d13abab3c
Cam McHenry <camchenry@users.noreply.github.com>
2026-06-01 00:32:24 -0400

    Remove accidental template

--- tests/conformance/cases/compiler/typeCast.ts
+++ tests/conformance/cases/compiler/typeCast.ts
@@ -3,7 +3,7 @@ declare const x: any;
 const xs = <string>x;
 const xu = <unknown>x;
 const xn = <number>x;
-const xa = <any>x;``
+const xa = <any>x;
 
 interface i00<T = number> { a: T; }
 const i00c00 = (<i00>x)
