009ac8a9dfae7772c1ec776bedaaadfadc8f1538
Cam McHenry <camchenry@users.noreply.github.com>
2026-06-15 23:21:27 -0400

    Add node to type check example

--- src/examples/type_check.rs
+++ src/examples/type_check.rs
@@ -37,7 +37,7 @@ fn run(path: PathBuf) -> Result<(), Box<dyn Error>> {
     let checker = CheckerBuilder::new().build(&store);
 
     for entry in store.entries().iter().filter(|entry| !entry.is_lib()) {
-        for (node_id, _) in entry.semantic().nodes().iter_enumerated() {
+        for (node_id, _node) in entry.semantic().nodes().iter_enumerated() {
             let _ = checker.get_type_at_location(NodeRef::new(entry.id(), node_id));
         }
     }
