f68be1180bed0656930ad2c86267632ad389bd17
Cam McHenry <camchenry@users.noreply.github.com>
2026-08-30 20:03:26 -0400

    Add asserts cases

--- tests/conformance/cases/compiler/functionReturnTypes.ts
+++ tests/conformance/cases/compiler/functionReturnTypes.ts
@@ -24,3 +24,23 @@ function returnsBig2() {
   }
   return 1n
 }
+
+function assertsIsString(x: unknown): asserts x is string {
+  if (typeof x !== "string") {
+    throw new Error("Not a string");
+  }
+}
+
+function isString(x: unknown): x is string {
+  return typeof x === "string";
+}
+
+function genericIsString<T>(x: T): x is T {
+  return typeof x === "string";
+}
+
+function assertsGenericIsString<T>(x: T): asserts x is T {
+  if (typeof x !== "string") {
+    throw new Error("Not a string");
+  }
+}
